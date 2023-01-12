304b16837005ae4eb0661355f102845ee2b72e40
FranD87 <fdunne007@gmail.com>
2023-01-12 08:44:39 +0100

    Commit

---
+++ exercise.py
@@ -0,0 +1,28 @@
+# Task 1
+#
+text = "Berlin is a world city of culture, politics, media and science."
+
+# print(len(text))
+
+# Task 2
+
+# print(text[0],text[-1])
+
+# Task 3
+
+# print(text[0:3].upper())
+
+# Task 4
+#
+# text = "Berlin is surrounded by the state of Brandenburg and contiguous with Potsdam, Brandenburg's capital."
+#
+# print("B appears:", text.count('B'), "times.")
+
+# Task 5
+
+# text = "Berlin straddles the banks of the spree, which flows into the Havel (a tributary of the Elbe) in the western borough of Spandau."
+#
+# print(text[-10:])
+
+# Task 6
+
